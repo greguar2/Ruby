4c6058526d427fa7e45256119aa8641133f5d4f3
Grégoire Marino <gregoire_marino@yahoo.fr>
2014-10-17 13:01:20 +0200

    compile database URL

--- new_ruby.rb
+++ new_ruby.rb
@@ -1,2 +1,7 @@
 require "sequel"
-DB = Sequel.connect('mysql://soundcloud_ro:pw@analytics-db.int.s-cloud.net:3306/soundcloud_production')
+require "json"
+
+file = File.open (".dbsecrets")
+secrets = JSON.parse file.read
+puts "mysql://" + secrets['user'] + ":" + secrets['password'] + secrets['host'] + ":" + secrets['port'] + "/" + secrets['database']
+#DB = Sequel.connect('mysql://soundcloud_ro:pw@analytics-db.int.s-cloud.net:3306/soundcloud_production')
